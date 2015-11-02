c6e4d355dbe69c2763b44f56941bae3292f27954
James Ward <james@jamesward.org>
2015-11-02 13:00:15 -0700

    bind to all interfaces

--- src/main/java/com/jamesward/hellojaxws/Server.java
+++ src/main/java/com/jamesward/hellojaxws/Server.java
@@ -1,16 +1,14 @@
 package com.jamesward.hellojaxws;
 
 import javax.xml.ws.Endpoint;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 
 public class Server {
 
-    public static void main(String[] args) throws MalformedURLException {
-        final String proto = System.getenv("PROTO") != null ? System.getenv("PROTO") : "http";
-        final String host = System.getenv("HOST") != null ? System.getenv("HOST") : "localhost";
+    public static void main(String[] args) throws IOException {
         final int port = System.getenv("PORT") != null ? Integer.valueOf(System.getenv("PORT")) : 8080;
-        final URL helloURL = new URL(proto, host, port, "/hello");
+        final URL helloURL = new URL("http", "0.0.0.0", port, "/hello");
 
         Endpoint.publish(helloURL.toString(), new HelloWorldService());
     }
